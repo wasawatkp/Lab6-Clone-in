b946d62aea2250b3945fc1579f21e094859aa3cf
wasawatkp <wasawat.upper@gmail.com>
2024-12-02 22:20:29 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int Number=3;
+    int evenNum=0;
+    int oddNum=0;
+   
+    while(Number != 0){
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> Number;
+
+    if(Number != 0 ){
+        if(Number%2 == 0 ){evenNum++;}
+        else{oddNum++;}
+
+    }
+   
+    
+    
+    }
+    
+    cout << "#Even numbers = " << evenNum;
+    cout << "#Odd numbers = " << oddNum;
     return 0;
 }
